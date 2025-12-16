ae7321482e412edff506980b9511a3f20a3d1c49
Bogdi896 <bogdi896@yahoo.com>
2025-12-16 03:33:45 +0200

    Added dependency injection, cleaned program.cs a bit

---
+++ FitnessTracker.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using FitnessTracker.Application.Interfaces;
+using FitnessTracker.Infrastructure.Data;
+using FitnessTracker.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FitnessTrackerDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("FitnessTrackerDb");
+                options.UseSqlServer(connectionString);
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
--- FitnessTracker.WebApi/Program.cs
+++ FitnessTracker.WebApi/Program.cs
@@ -1,27 +1,18 @@
-using FitnessTracker.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using FitnessTracker.Infrastructure.Repositories;
+using FitnessTracker.Infrastructure;
 using FitnessTracker.Application.Interfaces;
 using FitnessTracker.Application.Services;
 using FitnessTracker.WebApi.Middleware;
+using FitnessTracker.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<FitnessTrackerDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("FitnessTrackerDb")));
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddMemoryCache();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IWorkoutService, WorkoutService>();
-builder.Services.AddScoped<IExerciseService, ExerciseService>();
-builder.Services.AddScoped<IWorkoutExerciseService, WorkoutExerciseService>();
-builder.Services.AddScoped<IGoalService, GoalService>();
-builder.Services.AddScoped<IFoodItemService, FoodItemService>();
-builder.Services.AddScoped<IFoodLogService, FoodLogService>();
-builder.Services.AddScoped<IMeasurementLogService, MeasurementLogService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
